cc1e05242ebcc735cc324ac16e8ef099f36f8f3a
Gary Bergman <garybergman00@gmail.com>
2020-11-10 13:16:59 -0500

    Played around with destructuring props to create new robots from a JSON file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
-import Test from './Test';
+import roboJSON from './robo.json'
+// import Test from './Test';
 import CardComponent from './CardComponent'
 
-const App = (props) => {
+const App = () => {
   return (
     <>
       {/* <Test /> */}
-      <CardComponent />
+      <CardComponent id={roboJSON[0].id} name={roboJSON[0].name} email={roboJSON[0].email} />
+      <CardComponent id={roboJSON[1].id} name={roboJSON[1].name} email={roboJSON[1].email} />
+      <CardComponent id={roboJSON[2].id} name={roboJSON[2].name} email={roboJSON[2].email} />
+      <CardComponent id={roboJSON[3].id} name={roboJSON[3].name} email={roboJSON[3].email} />
+      <CardComponent id={roboJSON[4].id} name={roboJSON[4].name} email={roboJSON[4].email} />
+      <CardComponent id={roboJSON[5].id} name={roboJSON[5].name} email={roboJSON[5].email} />
+      <CardComponent id={roboJSON[6].id} name={roboJSON[6].name} email={roboJSON[6].email} />
+      <CardComponent id={roboJSON[7].id} name={roboJSON[7].name} email={roboJSON[7].email} />
     </>
   );
 }
--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './CardComponent.css';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-const CardComponent = (props) => {
+
+const CardComponent = ({ id, name, email }) => {
   return (
     <>
-      <Card className="cardComp hvr-grow">
-        <CardImg className="cardImage1" src="https://robohash.org/robo1" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-         
+      <Card className="cardComp hvr-grow shadow bg-info mt-5 ">
+        <CardImg className="cardImage1" src={`https://robohash.org/robo${id}`} alt="Card image cap" />
+        <CardBody className="cb">
+          <CardTitle tag="h5">{name}</CardTitle>
+          <CardText >{email}</CardText>
         </CardBody>
       </Card>
     </>
